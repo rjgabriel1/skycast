216103105d936b28a29361355d26c497e42d54d8
rjgabriel1 <gabrieljeffersonralph@gmail.com>
2024-10-03 23:09:19 -0400

    [ADD]: features to get user location and weather data

--- App.js
+++ App.js
@@ -3,10 +3,57 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import Home from "./pages/Home/Home";
 import { ImageBackground } from "react-native";
 import background from "./assets/background.png";
+import { useEffect, useState } from "react";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+} from "expo-location";
+
+import { WeatherAPI } from "./api/weather";
 
 export default function App() {
+  const [coordinates, setCoordinates] = useState();
+  const [weather, setWeather] = useState();
+  useEffect(() => {
+    getUserCoordinates();
+  }, []);
+
+  useEffect(() => {
+    if (coordinates) {
+      fetchWeatherFromCoords(coordinates);
+    }
+  }, [coordinates]);
+
+async function fetchWeatherFromCoords(coordinates) {
+  try {
+    const weatherRes = await WeatherAPI.fetchWeatherByCoords(coordinates);
+    console.log(weatherRes);
+    setWeather(weatherRes);
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    setWeather(null); // or handle the error state as needed
+  }
+}
+
+  async function getUserCoordinates() {
+    const { status } = await requestForegroundPermissionsAsync();
+    if (status === "granted") {
+      const location = await getCurrentPositionAsync();
+      setCoordinates({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude,
+      });
+    } else {
+      setCoordinates({ lat: "48.85", lng: "2.35" });
+    }
+  }
+
   return (
-    <ImageBackground source={background} style={s.img_background} imageStyle={s.img}>
+    <ImageBackground
+      source={background}
+      style={s.img_background}
+      imageStyle={s.img}
+    >
       <SafeAreaProvider>
         <SafeAreaView style={s.container}>
           <Home />
