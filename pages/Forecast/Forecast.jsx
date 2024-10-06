259b5a32993297d0ddf890d36ec24af170cc8dbd
rjgabriel1 <gabrieljeffersonralph@gmail.com>
2024-10-06 14:29:02 -0400

    add forecast page

---
+++ pages/Forecast/Forecast.jsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import Txt from "../../components/Txt/Txt";
+import { s } from "./Forecast.style";
+export default function Forecast() {
+  return (
+    <View>
+      <Txt>Forecast</Txt>
+    </View>
+  );
+}
