782d1deb60af69bd40ed8832a76856917bcea577
rjgabriel1 <gabrieljeffersonralph@gmail.com>
2024-10-06 13:02:04 -0400

    [Add]: weather advanced section component

---
+++ components/WeatherAdvanced/WeatherAdvanced.jsx
@@ -0,0 +1,21 @@
+import { View } from "react-native";
+import Txt from "../Txt/Txt";
+import { s } from "./weatherAdvanced.style";
+export default function WeatherAdvanced() {
+  return (
+    <View>
+      <View>
+        <Txt></Txt>
+        <Txt></Txt>
+      </View>
+      <View>
+        <Txt></Txt>
+        <Txt></Txt>
+      </View>
+      <View>
+        <Txt></Txt>
+        <Txt></Txt>
+      </View>
+    </View>
+  );
+}
