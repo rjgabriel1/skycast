7a26e5d134b2fe5c3cc0d333a8d8068a76a9961b
rjgabriel1 <gabrieljeffersonralph@gmail.com>
2024-10-06 13:36:08 -0400

    add styling to the advanced component

--- components/WeatherAdvanced/WeatherAdvanced.jsx
+++ components/WeatherAdvanced/WeatherAdvanced.jsx
@@ -1,21 +1,21 @@
 import { View } from "react-native";
 import Txt from "../Txt/Txt";
-import { s } from "./weatherAdvanced.style";
-export default function WeatherAdvanced() {
+import { StyledContainer, StyledLabel, StyledValue, s } from "./weatherAdvanced.style";
+export default function WeatherAdvanced({ sunrise, sunset, windSpeed }) {
   return (
-    <View>
-      <View>
-        <Txt></Txt>
-        <Txt></Txt>
-      </View>
-      <View>
-        <Txt></Txt>
-        <Txt></Txt>
-      </View>
-      <View>
-        <Txt></Txt>
-        <Txt></Txt>
-      </View>
+    <View style={s.advanced_container}>
+      <StyledContainer>
+        <StyledValue>123{sunrise}</StyledValue>
+        <StyledLabel>Sunrise</StyledLabel>
+      </StyledContainer>
+      <StyledContainer>
+        <StyledValue>123{sunset}</StyledValue>
+        <StyledLabel>Sunset</StyledLabel>
+      </StyledContainer>
+      <StyledContainer>
+        <StyledValue>123{windSpeed}</StyledValue>
+        <StyledLabel>Wind speed</StyledLabel>
+      </StyledContainer>
     </View>
   );
 }
--- components/WeatherAdvanced/weatherAdvanced.style.js
+++ components/WeatherAdvanced/weatherAdvanced.style.js
@@ -1,3 +1,29 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
+import Txt from "../Txt/Txt";
 
-export const s =StyleSheet.create({})
+const s = StyleSheet.create({
+  advanced_container: {
+    flexDirection: "row",
+    flex: 1,
+    backgroundColor: "#00000063",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    borderColor: "white",
+    borderWidth: 1,
+    borderRadius: 10,
+  },
+});
+
+function StyledContainer({ children }) {
+  return <View style={{ alignItems: "center" }}>{children}</View>;
+}
+function StyledLabel({ children }) {
+  return <Txt style={{ fontSize: 15 }}>{children}</Txt>;
+}
+function StyledValue({ children }) {
+  return <Txt style={{ fontSize: 20 }}>{children}</Txt>;
+}
+
+
+
+export { s, StyledContainer, StyledLabel, StyledValue };
