3beabb1f0e0f1fb6ef2a5b0d1cf92d99c08e8b13
rjgabriel1 <gabrieljeffersonralph@gmail.com>
2024-10-07 20:00:54 -0400

    add push notification

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { s } from "./App.style";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import Home from "./pages/Home/Home";
 import Forecast from "./pages/Forecast/Forecast";
-import { Alert, ImageBackground } from "react-native";
+import { Alert, ImageBackground, Platform } from "react-native";
 import background from "./assets/background.png";
 import { useEffect, useState } from "react";
 import {
@@ -13,6 +13,10 @@ import { useFonts } from "expo-font";
 import { WeatherAPI } from "./api/weather";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as Notifications from "expo-notifications";
+import * as Device from "expo-device";
+import Constants from "expo-constants";
+
 
 const Stack = createNativeStackNavigator();
 const navTheme = {
@@ -31,6 +35,7 @@ export default function App() {
 
   useEffect(() => {
     getUserCoordinates();
+    registerForNotifications();
   }, []);
 
   useEffect(() => {
@@ -39,6 +44,46 @@ export default function App() {
       fetchCityFromCoords(coordinates);
     }
   }, [coordinates]);
+
+  async function registerForNotifications() {
+    let token;
+
+    if (Platform.OS === "android") {
+      try {
+        await Notifications.setNotificationChannelAsync("default", {
+          name: "default",
+          importance: Notifications.AndroidImportance.MAX,
+          vibrationPattern: [0, 250, 250, 250],
+          lightColor: "#FF231F7C",
+        });
+      } catch (error) {}
+    }
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
+      }
+      token = (
+        await Notifications.getExpoPushTokenAsync({
+          projectId:
+            Constants?.expoConfig?.extra?.eas?.projectId ??
+            Constants?.easConfig?.projectId,
+        })
+      ).data;
+      console.log("token", token);
+    } else {
+      alert("Must use physical device for Push Notifications");
+    }
+
+    return token;
+  }
 
   async function fetchWeatherFromCoords(coords) {
     try {
